c3cf4c8ad3c4c250f6385ebac859c7c0d7d24306
samuelteyzier <acs.samuel.mania@gmail.com>
2018-03-16 10:26:55 +0100

    Bit-win

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BIT-WIN !</title>
+</head>
+<body>
+    <?php
+    include "viewUpload.html";
+    include "viewDownload.html";
+    include "viewFixed.html";
+    include "controler.html";
+    include "viewConfirm.html"
+    ?>
+</body>
+</html>
