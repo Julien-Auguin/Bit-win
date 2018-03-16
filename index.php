d0c24f9e9bd047c1061d2cea0d671927eb109728
Caroline Astier <caroline.duryc@gmail.com>
2018-03-16 10:09:33 +0100

    Add css ViewFixed and ViewUpload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" media="screen and (max-width: 780px)" href="css/phone.css">
+    <link rel="stylesheet" media="screen and (min-width: 781px)" href="css/screen.css">
+    <link rel="stylesheet" href="css/position.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>BIT-WIN !</title>
+</head>
+<body>
+    <?php
+    // include "viewUpload.php";
+    // include "viewDownload.php";
+    include "viewFixed.php";
+    // include "controler.php";
+    // include "viewConfirm.php";
+    ?>
+</body>
+</html>
