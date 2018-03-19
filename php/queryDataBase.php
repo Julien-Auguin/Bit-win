2c072c37da83d6a7a5bd5159857e7ccca1b7cc9c
Caroline Astier <caroline.duryc@gmail.com>
2018-03-19 17:02:22 +0100

    add insert files in table File_Upload

--- php/queryDataBase.php
+++ php/queryDataBase.php
@@ -2,6 +2,9 @@
 
 <?php
 
+$total = count($_FILES['select']['name']);
+echo $total;
+
     // Function insertUser in table Users
     function insertUser(PDO $conn,$userMail,$nameUser,$message){
         $user_mail = $userMail;
@@ -25,12 +28,13 @@
 
 
     if((isset($_POST['mail-sender'])) && ($_POST['mail-sender'] <> '') 
-        && (isset($_POST['recipient'])) && ($_POST['recipient'] <> '')){
+        && (isset($_POST['recipient'])) && ($_POST['recipient'] <> '')
+        && (isset($_FILES['select'])) && ($_FILES['select']['name'] <> '')){
             
         // Insertion Sender in DataBase
         if((isset($_POST['mail-sender'])) && ($_POST['mail-sender'] <> '')){
-            include ('php/param.php');
-            include ('php/connectionDB.php');
+            require 'php/param.php';
+            require 'php/connectionDB.php';
             insertUser($conn,$_POST["mail-sender"],$_POST["name"], "sender");
             unset($_POST["name"]);
             unset($_POST["mail-sender"]);
@@ -38,18 +42,61 @@
 
         // Insertion Recipient in DataBase               
         if((isset($_POST['recipient'])) && ($_POST['recipient'] <> '')){
-            include ('php/param.php');
-            include ('php/connectionDB.php');
+            require 'php/param.php';
+            require 'php/connectionDB.php';
             insertUser($conn,$_POST["recipient"],"","recipient");
             unset($_POST["recipient"]);
         }
+           
+        
+        // Insertion files in table Files_Upload
+        if((isset($_FILES['select'])) && ($_FILES['select']['name'] <> '')){
+            
+                require 'php/param.php';
+                require 'php/connectionDB.php';
+                
+                // count the lenght of array $_FILES
+                $total = count($_FILES['select']['name']);
+                echo $total;
+    
+                // Loop through each file
+                for($i=0; $i<$total; $i++) {    
+                    $file_name = $_FILES['select']['name'][$i];
+                    // echo "filename = ". $file_name;
+                    $file_size = $_FILES['select']['size'][$i];
+                    // $directory_name = ;
+                    $requete2 = $conn->prepare(
+                        "INSERT INTO File_Upload (file_name, file_size, directory_name)
+                        VALUES  (:file_name, :file_size,:directory_name)"
+                        );
+                        $requete2->bindParam(':file_name', $file_name);
+                        $requete2->bindParam(':file_size', $file_size);
+                        $requete2->bindParam(':directory_name', $directory_name);
+                        if ((isset($file_name)) && ($file_name <> "")){
+                            $result2 = $requete2->execute();
+                            echo "<em>Insertion file OK.</em>";
+                        }
+                        else {
+                            echo "<em>Please select file </em>";
+                        }
+                }
+                    $_FILES = "";
+            }
+            else echo "Pas de fichier";
     }
-    else if ($_POST['mail-sender'] == ''){
-        echo "Please enter sender mail";
+    else {
+        if (!isset($_POST['mail-sender'])){
+        echo "<em>Please enter sender mail </em>";
+        }   
+        if (!isset($_POST['recipient'])){
+        echo "<em>Please enter recipient mail </em>";
+        }
+        if (!isset($_FILES['select'])){
+        echo "<em>Please select files </em>";
+        }    
     }
-    else if ($_POST['recipient'] == ''){
-        echo "Please enter recipient mail";
-    }
+
+
 
     // TO DO: 
     // 1. vérifier si les User sont déjà insérés dans la BDD
